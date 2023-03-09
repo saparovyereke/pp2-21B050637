26e7f15ac5dbf033e6f0c6eb99428e89fd70fcd6
saparovyereke <123382638+saparovyereke@users.noreply.github.com>
2023-03-09 12:28:51 +0600

    Update 3.py

--- tsis6/files/3.py
+++ tsis6/files/3.py
@@ -1,5 +1,5 @@
 import os
-x = "C:\ПП1\code Python 2 semester\lab6"
+x = "C:\git\pp2-21B050637\sis6"
 if os.access(x, os.F_OK):
     print("direcroty:", os.path.dirname(x))
     print("name:", os.path.basename(x))
