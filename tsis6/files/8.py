c9bb1ea28256811dd5dff81914b9316360d11dc0
saparovyereke <123382638+saparovyereke@users.noreply.github.com>
2023-03-09 12:31:54 +0600

    Update 8.py

--- tsis6/files/8.py
+++ tsis6/files/8.py
@@ -1,6 +1,6 @@
 import os
 
-if os.path.exists("lab6/files/file_for_delete.txt"):
-    os.remove("lab6/files/file_for_delete.txt")
+if os.path.exists("sis6/files/file_for_delete.txt"):
+    os.remove("sis6/files/file_for_delete.txt")
 else:
     print("file does not exist")
