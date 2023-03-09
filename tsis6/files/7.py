ac6886976824bcc54fb19c85ed4c24cfc8fc26b4
saparovyereke <123382638+saparovyereke@users.noreply.github.com>
2023-03-09 12:31:24 +0600

    Update 7.py

--- tsis6/files/7.py
+++ tsis6/files/7.py
@@ -1,5 +1,5 @@
-f = open("lab6/files/copy_from.txt", "r")
-f1 = open("lab6/files/copy_to.txt", "w")
+f = open("sis6/files/copy_from.txt", "r")
+f1 = open("sis6/files/copy_to.txt", "w")
 for x in f:
     f1.write(x)
 f.close()
