c19a085be1b1bd78e9d43a807ddd9b782c7f5e4e
saparovyereke <123382638+saparovyereke@users.noreply.github.com>
2023-03-09 12:28:20 +0600

    Update 2.py

--- tsis6/files/2.py
+++ tsis6/files/2.py
@@ -1,6 +1,6 @@
 import os
 
-print("exist:", os.access("tsis6", os.F_OK))
-print("read:", os.access("tsis6", os.R_OK))
-print("write:", os.access("tsis6", os.W_OK))
-print("execute:", os.access("tsis6", os.X_OK))
+print("exist:", os.access("sis6", os.F_OK))
+print("read:", os.access("sis6", os.R_OK))
+print("write:", os.access("sis6", os.W_OK))
+print("execute:", os.access("sis6", os.X_OK))
